e61ce47fb69dfd204a9c0bbbe3035c5d6146c032
HedrichPio <61136433+HedrichPio@users.noreply.github.com>
2021-07-14 11:32:34 +0530

    added first native iOS app Xylophone

---
+++ Tutorials/Xylophone-iOS13/Xylophone/ViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        var note = sender.currentTitle!
+    
+        playSound(soundName:note)
+    }
+    
+    func playSound(soundName:String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
